7bff75efd8fe95687b50fafa6c65494cd1d99d47
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-12 21:53:10 +0900

    feat : 평일 할인 코드 작성

--- src/main/java/discount/WeekdayDiscount.java
+++ src/main/java/discount/WeekdayDiscount.java
@@ -1,16 +1,38 @@
 package discount;
 
 import java.util.HashMap;
+import java.util.Set;
+
+import christmas.Menu;
 
 public class WeekdayDiscount {
+	private final Set<Integer> datesWeekday = Set.of(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26,
+			27, 28, 31);
+	private final int AMOUNT = 2023;
+	private final String TYPE = "디저트";
+	private int date;
+	private HashMap<Menu, Integer> inputMenu = new HashMap<Menu, Integer>();
 
-	public WeekdayDiscount(int testDate, HashMap<String, Integer> testMenu) {
-		// TODO Auto-generated constructor stub
+	public WeekdayDiscount(int date, HashMap<Menu, Integer> inputMenu) {
+		this.date = date;
+		this.inputMenu = inputMenu;
+	}
+
+	private boolean isWeekday() {
+		return datesWeekday.contains(date);
 	}
 
 	public int calculate() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		if (!isWeekday()) {
+			return 0;
+		}
 
+		int count = 0;
+		for (Menu key : inputMenu.keySet()) {
+			if (key.type().equals(TYPE)) {
+				count = count + inputMenu.get(key);
+			}
+		}
+		return AMOUNT * count;
+	}
 }
--- src/test/java/discount/WeekdayDiscountTest.java
+++ src/test/java/discount/WeekdayDiscountTest.java
@@ -10,14 +10,14 @@ import christmas.Menu;
 
 class WeekdayDiscountTest {
 	private int testDate;
-	private HashMap<String, Integer> testMenu = new HashMap<String, Integer>();
+	private HashMap<Menu, Integer> testMenu = new HashMap<Menu, Integer>();
 
 	@DisplayName("평일 할인 테스트 - 평일 디저트 1, 메인 1")
 	@Test
 	void testCalculate1() {
 		testDate = 3;
-		testMenu.put(Menu.초코케이크.toString(), 1);
-		testMenu.put(Menu.바비큐립.toString(), 1);
+		testMenu.put(Menu.초코케이크, 1);
+		testMenu.put(Menu.바비큐립, 1);
 		WeekdayDiscount testWeekday = new WeekdayDiscount(testDate, testMenu);
 
 		assertThat(testWeekday.calculate()).isEqualTo(2023);
@@ -27,8 +27,8 @@ class WeekdayDiscountTest {
 	@Test
 	void testCalculate2() {
 		testDate = 3;
-		testMenu.put(Menu.초코케이크.toString(), 2);
-		testMenu.put(Menu.바비큐립.toString(), 1);
+		testMenu.put(Menu.초코케이크, 2);
+		testMenu.put(Menu.바비큐립, 1);
 		WeekdayDiscount testWeekday = new WeekdayDiscount(testDate, testMenu);
 
 		assertThat(testWeekday.calculate()).isEqualTo(2023 * 2);
@@ -37,9 +37,9 @@ class WeekdayDiscountTest {
 	@DisplayName("평일 할인 테스트 - 주말 디저트 1, 메인 1")
 	@Test
 	void testCalculate3() {
-		testDate = 3;
-		testMenu.put(Menu.초코케이크.toString(), 1);
-		testMenu.put(Menu.바비큐립.toString(), 1);
+		testDate = 1;
+		testMenu.put(Menu.초코케이크, 1);
+		testMenu.put(Menu.바비큐립, 1);
 		WeekdayDiscount testWeekday = new WeekdayDiscount(testDate, testMenu);
 
 		assertThat(testWeekday.calculate()).isEqualTo(0);
