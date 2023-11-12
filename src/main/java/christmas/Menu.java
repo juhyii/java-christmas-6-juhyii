e0de3833345bf706995ce95e0f7df69690f90252
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-12 20:48:15 +0900

    feat : Menu Enum 생성

---
+++ src/main/java/christmas/Menu.java
@@ -0,0 +1,33 @@
+package christmas;
+
+public enum Menu {
+	양송이수프("애피타이저", 6000),
+	타파스("애피타이저", 5500),
+	시저샐러드("애피타이저", 8000),
+	티본스테이크("메인", 55000),
+	바비큐립("메인", 54000),
+	해산물파스타("메인", 35000),
+	크리스마스파스타("메인", 25000),
+	초코케이크("디저트", 15000),
+	아이스크림("디저트", 5000),
+	제로콜라("음료", 3000),
+	레드와인("음료", 60000),
+	샴페인("음료", 25000);
+	
+	private String type;
+	private int price;
+
+	Menu(String type, int price) {
+		this.type = type;
+		this.price = price;
+	}
+
+	public String type() {
+		return this.type;
+	}
+	
+	public int price() {
+		return this.price;
+	}
+
+}
