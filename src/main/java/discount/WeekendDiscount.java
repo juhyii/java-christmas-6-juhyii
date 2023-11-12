97e4e2cf784e0440bec8678e816c70d11dbbd454
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-12 22:08:12 +0900

    feat : 주말 할인 코드 작성

--- src/main/java/discount/WeekendDiscount.java
+++ src/main/java/discount/WeekendDiscount.java
@@ -1,17 +1,37 @@
 package discount;
 
 import java.util.HashMap;
+import java.util.Set;
+
 import christmas.Menu;
 
 public class WeekendDiscount {
+	private final Set<Integer> datesWeekend = Set.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+	private final int AMOUNT = 2023;
+	private final String TYPE = "메인";
+	private int date;
+	private HashMap<Menu, Integer> inputMenu = new HashMap<Menu, Integer>();
 
-	public WeekendDiscount(int testDate, HashMap<Menu, Integer> testMenu) {
-		// TODO Auto-generated constructor stub
+	public WeekendDiscount(int date, HashMap<Menu, Integer> inputMenu) {
+		this.date = date;
+		this.inputMenu = inputMenu;
+	}
+
+	private boolean isWeekday() {
+		return datesWeekend.contains(date);
 	}
 
 	public int calculate() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		if (!isWeekday()) {
+			return 0;
+		}
 
+		int count = 0;
+		for (Menu key : inputMenu.keySet()) {
+			if (key.type().equals(TYPE)) {
+				count = count + inputMenu.get(key);
+			}
+		}
+		return AMOUNT * count;
+	}
 }
