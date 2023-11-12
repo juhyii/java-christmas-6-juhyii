c3453441b43bb44cb346d07ac5b336778f55f20d
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-12 22:31:43 +0900

    test : 증정 이벤트 테스트 코드 작성

---
+++ src/main/java/discount/GiveawayEvent.java
@@ -0,0 +1,14 @@
+package discount;
+
+public class GiveawayEvent {
+
+	public GiveawayEvent(int totalAmount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int calculate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
