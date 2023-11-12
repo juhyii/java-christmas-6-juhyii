7ed923581a61250b6d496b8389a2af27377c90e5
jhyii <83736896+jhyii@users.noreply.github.com>
2023-11-12 12:39:41 +0900

    feat : 디데이 할인 코드 작성

--- src/main/java/discount/DDayDiscount.java
+++ src/main/java/discount/DDayDiscount.java
@@ -1,9 +1,26 @@
 package discount;
 
-public class DDayDiscount {
+import java.util.HashMap;
 
-	public int calculate(int date) {
-		return 0;
+public class DDayDiscount {
+	private HashMap<Integer, Integer> discountCalendar;
+
+	public DDayDiscount() {
+		generateCalendar();
 	}
 
+	private void generateCalendar() {
+		int amount = 1000;
+		for (int i = 1; i <= 25; i++) {
+			discountCalendar.put(i, amount);
+			amount = amount + 100;
+		}
+	}
+
+	public int calculate(int date) {
+		if (date > 25) {
+			return 0;
+		}
+		return discountCalendar.get(date);
+	}
 }
